65bfd1a920d64ee6b84b53d77d342fa34309b028
James Foxlee <james.foxlee@pm.me>
2025-03-11 18:34:03 +0000

    Step 1 - add structs / other types for model

--- src/models.rs
+++ src/models.rs
@@ -1,28 +1,49 @@
+use std::collections::HashMap;
+
+pub type Id = u32;
 pub enum Status {
-    // TODO: add fields (make sure the fields are public)
+    Open,
+    InProgress,
+    Resolved,
+    Closed
 }
 
 pub struct Epic {
-    // TODO: add fields (make sure the fields are public)
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<Id>,
 }
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open and the stories should be an empty vector
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: Vec::new(),
+        }
     }
 }
 
 pub struct Story {
-    // TODO: add fields (make sure the fields are public)
+    pub name: String,
+    pub description: String,
+    pub status: Status,
 }
 
 impl Story {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open
+        Self {
+            name,
+            description,
+            status: Status::Open,
+        }
     }
 }
 
 pub struct DBState {
-    // This struct represents the entire db state which includes the last_item_id, epics, and stories
-    // TODO: add fields (make sure the fields are public)
+    last_item_id: Id,
+    epics: HashMap<Id, Epic>,
+    stories: HashMap<Id, Story>,
 }
